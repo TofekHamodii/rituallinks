534c2dfb71b8900eca2ddfd2e0da25eb9bd58222
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-10-27 20:49:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,27 +51,33 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
 }
 });
 
-var prefix = "-"
-client.on('message', message => { //invite
-    if (message.content.startsWith(prefix + "رابط")) {
-     if(!message.channel.guild) return;
-if (message.author.bot) return;
-        message.channel.createInvite({
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+ 
+  message.channel.createInvite({
         thing: true,
-        maxUses: 0,
+        maxUses: 100,
         maxAge: 86400
     }).then(invite =>
       message.author.sendMessage(invite.url)
     )
-    const Embed11 = new Discord.RichEmbed()
-        .setColor("#5016f3")
-        .setDescription("تم ارسالك في الخاص")
-   .setFooter("اسم سيررك ",'رابط صوره سيرفرك')
-                   .setTimestamp()
-                message.channel.send('**تم الارسال رابط  سيرفر في الخاص**');
-
-
-      message.channel.sendEmbed(Embed11).then(message => {message.delete(3000)})
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+                .setAuthor(message.guild.name, message.guild.iconURL)
+        .setDescription(`
+**
+---------------------
+-[${message.guild.name}]  هذا هو رابط سيرفر
+---------------------
+-هذا الرابط صالح ل 100 مستخدم فقط
+---------------------
+-هذا الرابط صالح لمده 24 ساعه فقط
+---------------------
+**`)
       message.author.sendEmbed(Embed11)
     }
 });
